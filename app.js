0f2861117b42d234656e68e5fe6abab466efbfda
AbelRodriguez199 <abelrodriguez199@gmail.com>
2019-07-05 17:54:08 +0200

    Cors Añadido

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-const cors         = require('cors');
+const createError  = require('http-errors')
 
 
 
@@ -17,19 +17,21 @@ const cors         = require('cors');
 
 // const session       = require('express-session');
 // const passport      = require('passport');
-
+require('./configs/db.config')
 // require('./configs/passport');
+// require('./configs/session.config');
+const cors  = require('./configs/cors.config');
 
 // IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
 
-mongoose
-  .connect('mongodb://localhost/menu-planner', {useNewUrlParser: true})
+/*mongoose
+  .connect('mongodb://localhost/menu-planner-api', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
-  });
+  });*/
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -38,26 +40,22 @@ const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
+    
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // ADD SESSION SETTINGS HERE:
-
+app.use((req, res, next) => {
+  res.locals.session = req.user;
+  next();
+})
 
 // USE passport.initialize() and passport.session() HERE:
 
@@ -68,10 +66,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 // ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
 
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
-}));
+app.use(cors);
 
 // ROUTES MIDDLEWARE STARTS HERE:
 app.use('/api', require('./routes/category-routes'));
@@ -79,6 +74,29 @@ app.use('/api', require('./routes/recipe-routes'));
 app.use('/api', require('./routes/ingredient-routes'));
 const index = require('./routes/index');
 app.use('/', index);
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use((error, req, res, next) => {
+  console.error(error);  
+  res.status(error.status || 500);
+  const data = {};
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    res.status(400);
+    data.errors = {}
+    Object.keys(error.errors)
+      .forEach(field => data.errors[field] = error.errors[field].message)
+  } else if (error instanceof mongoose.Error.CastError) {
+    res.status(404);
+    error.message = 'Resource not found';
+  }
+
+  data.message = error.message
+  res.json(data);
+})
 
 
 module.exports = app;
